6ac5ff7d9f47c91b17f0eec06eb29f8624f94b9e
yamadicu <dicutia@gmail.com>
2023-05-03 18:48:26 +0200

    fine parte riassuntiva

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@
   </div>
 
   <!-- MERCHANDISE -->
-  <div class="bg-blue">
+  <section class="bg-blue">
 
     <div class="merch">
 
@@ -84,7 +84,119 @@
 
     </div>
 
-  </div>
+  </section>
+
+  <section id="riassunto">
+
+    <div id="last">
+
+      <div id="liste">
+
+        <div>
+
+          <ul>
+            <span class="titolo-lista white">DC COMICS</span>
+            <li>
+              <a href="#">Characters</a>
+            </li>
+            <li>
+              <a href="#">Comics</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">Games</a>
+            </li>
+            <li>
+              <a href="#">Videos</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+          </ul>
+
+          <ul>
+            <span class="titolo-lista white">SHOP</span>
+            <li>
+              <a href="#">Shop DC</a>
+            </li>
+            <li>
+              <a href="#">Shop DC Collectibles</a>
+            </li>
+          </ul>
+
+        </div>
+
+        <ul>
+          <span class="titolo-lista white">DC</span>
+          <li>
+            <a href="#">Terms Of Use</a>
+          </li>
+          <li>
+            <a href="#">Privacy policy (New)</a>
+          </li>
+          <li>
+            <a href="#">Ad Choices</a>
+          </li>
+          <li>
+            <a href="#">Advertising</a>
+          </li>
+          <li>
+            <a href="#">Jobs</a>
+          </li>
+          <li>
+            <a href="#">Subscription</a>
+          </li>
+          <li>
+            <a href="#">Talent Workshops</a>
+          </li>
+          <li>
+            <a href="#">CPSC Certificates</a>
+          </li>
+          <li>
+            <a href="#">Ratings</a>
+          </li>
+          <li>
+            <a href="#">Shop Help</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+
+        <ul>
+          <span class="titolo-lista white">SITES</span>
+          <li>
+            <a href="#">DC</a>
+          </li>
+          <li>
+            <a href="#">MAD Magazine</a>
+          </li>
+          <li>
+            <a href="#">DC Kids</a>
+          </li>
+          <li>
+            <a href="#">DC Universe</a>
+          </li>
+          <li>
+            <a href="#">DC Pover Visa</a>
+          </li>
+        </ul>
+
+
+      </div>
+
+      <div id="logo">
+        <img src="/img/dc-logo-bg.png" alt="">
+      </div>
+
+    </div>
+
+  </section>
 </template>
 
 <style lang="scss">
