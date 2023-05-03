a0f59bfe7ba335e802b302bd6278b5bf84b5ac80
yamadicu <dicutia@gmail.com>
2023-05-03 17:17:32 +0200

    creazione content

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,50 @@
 </script>
 
 <template>
-  <div>
+  <!-- NAVBAR -->
+  <nav>
+    <img src="/img/dc-logo.png" alt="logo dc">
 
+    <ul>
+      <li>
+        <a href="#">CHARACTERS</a>
+      </li>
+      <li>
+        <a href="#">COMICS</a>
+      </li>
+      <li>
+        <a href="#">MOVIES</a>
+      </li>
+      <li>
+        <a href="#">TV</a>
+      </li>
+      <li>
+        <a href="#">GAMES</a>
+      </li>
+      <li>
+        <a href="#">COLLECTIBLES</a>
+      </li>
+      <li>
+        <a href="#">VIDEOS</a>
+      </li>
+      <li>
+        <a href="#">FANS</a>
+      </li>
+      <li>
+        <a href="#">NEWS</a>
+      </li>
+      <li>
+        <a href="#">SHOP</a>
+      </li>
+    </ul>
+  </nav>
+
+  <!-- CONTENT -->
+  <div class="content">
+    <h1><a href="#">Content gose here</a></h1>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use './style/main.scss' as *;
+</style>
